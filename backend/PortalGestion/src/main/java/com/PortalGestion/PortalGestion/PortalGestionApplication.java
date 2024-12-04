49ffc272ca011b46aa817f47652efa7af7967b71
Miguel <miguelangelnunezlopez96@gmail.com>
2024-12-04 12:48:50 +0100

    first commit

---
+++ backend/PortalGestion/src/main/java/com/PortalGestion/PortalGestion/PortalGestionApplication.java
@@ -0,0 +1,13 @@
+package com.PortalGestion.PortalGestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalGestionApplication.class, args);
+	}
+
+}
---
+++ backend/PortalGestion/src/test/java/com/PortalGestion/PortalGestion/PortalGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PortalGestion.PortalGestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
