86eb886f74ce8947819f9ce42ee4d5660e3a92ba
Pravos94 <alvaropr69@gmail.com>
2025-01-10 20:38:50 +0100

    fix endpoint back proyecto

--- src/components/proyecto/ListProyectos.jsx
+++ src/components/proyecto/ListProyectos.jsx
@@ -4,6 +4,7 @@ import Navbar from "../NavBar";
 import proyectoServiceInstance from "../services/ProyectoService";
 import Paginacion from "../utils/Paginacion";
 import Filter from "../utils/Filter";
+import axios from "axios";
 
 // const proyectos = [
 //   {
@@ -32,101 +33,103 @@ import Filter from "../utils/Filter";
 
 const ListProyectos = () => {
   const navigate = useNavigate();
-  // const [proyectos, setProyectos] = useState([]); // comentado hasta quitar la constante de arriba
+  const [proyectos, setProyectos] = useState([]); // comentado hasta quitar la constante de arriba
   const [currentPage, setCurrentPage] = useState(0);
   const itemsPerPage = 6;
 
-  const proyectos = [
-    {
-      nombre: "Proyecto Alpha",
-      descripcion: "Desarrollo de una aplicación web para gestión de tareas.",
-      nivel: "Experto",
-      estrellas: 5,
-      foto: "/path/to/alpha.jpg",
-    },
-    {
-      nombre: "Proyecto Beta",
-      descripcion: "Análisis de datos de ventas para una empresa minorista.",
-      nivel: "Experto",
-      estrellas: 4,
-      foto: "/path/to/beta.jpg",
-    },
-    {
-      nombre: "Proyecto Gamma",
-      descripcion: "Implementación de un sistema de inteligencia artificial.",
-      nivel: "Experto",
-      estrellas: 4,
-      foto: "/path/to/gamma.jpg",
-    },
-    {
-      nombre: "Proyecto Delta",
-      descripcion: "Desarrollo de una plataforma de comercio electrónico.",
-      nivel: "Experto",
-      estrellas: 5,
-      foto: "/path/to/delta.jpg",
-    },
-    {
-      nombre: "Proyecto Epsilon",
-      descripcion: "Creación de un sistema de gestión de inventarios.",
-      nivel: "Experto",
-      estrellas: 4,
-      foto: "/path/to/epsilon.jpg",
-    },
-    {
-      nombre: "Proyecto Zeta",
-      descripcion: "Desarrollo de una aplicación móvil para fitness.",
-      nivel: "Experto",
-      estrellas: 4,
-      foto: "/path/to/zeta.jpg",
-    },
-    {
-      nombre: "Proyecto Eta",
-      descripcion: "Implementación de un CRM para una empresa de servicios.",
-      nivel: "Experto",
-      estrellas: 5,
-      foto: "/path/to/eta.jpg",
-    },
-    {
-      nombre: "Proyecto Theta",
-      descripcion: "Análisis de datos financieros para una institución bancaria.",
-      nivel: "Experto",
-      estrellas: 4,
-      foto: "/path/to/theta.jpg",
-    },
-    {
-      nombre: "Proyecto Iota",
-      descripcion: "Desarrollo de un sistema de reservas para un hotel.",
-      nivel: "Experto",
-      estrellas: 4,
-      foto: "/path/to/iota.jpg",
-    },
-    {
-      nombre: "Proyecto Kappa",
-      descripcion: "Creación de una plataforma de aprendizaje en línea.",
-      nivel: "Experto",
-      estrellas: 5,
-      foto: "/path/to/kappa.jpg",
-    },
-    {
-      nombre: "Proyecto Lambda",
-      descripcion: "Desarrollo de una aplicación de seguimiento de salud.",
-      nivel: "Experto",
-      estrellas: 4,
-      foto: "/path/to/lambda.jpg",
-    },
-    {
-      nombre: "Proyecto Mu",
-      descripcion: "Implementación de un sistema de gestión de recursos humanos.",
-      nivel: "Experto",
-      estrellas: 4,
-      foto: "/path/to/mu.jpg",
-    },
-  ];
+  // const proyectos = [
+  //   {
+  //     nombre: "Proyecto Alpha",
+  //     descripcion: "Desarrollo de una aplicación web para gestión de tareas.",
+  //     nivel: "Experto",
+  //     estrellas: 5,
+  //     foto: "/path/to/alpha.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Beta",
+  //     descripcion: "Análisis de datos de ventas para una empresa minorista.",
+  //     nivel: "Experto",
+  //     estrellas: 4,
+  //     foto: "/path/to/beta.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Gamma",
+  //     descripcion: "Implementación de un sistema de inteligencia artificial.",
+  //     nivel: "Experto",
+  //     estrellas: 4,
+  //     foto: "/path/to/gamma.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Delta",
+  //     descripcion: "Desarrollo de una plataforma de comercio electrónico.",
+  //     nivel: "Experto",
+  //     estrellas: 5,
+  //     foto: "/path/to/delta.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Epsilon",
+  //     descripcion: "Creación de un sistema de gestión de inventarios.",
+  //     nivel: "Experto",
+  //     estrellas: 4,
+  //     foto: "/path/to/epsilon.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Zeta",
+  //     descripcion: "Desarrollo de una aplicación móvil para fitness.",
+  //     nivel: "Experto",
+  //     estrellas: 4,
+  //     foto: "/path/to/zeta.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Eta",
+  //     descripcion: "Implementación de un CRM para una empresa de servicios.",
+  //     nivel: "Experto",
+  //     estrellas: 5,
+  //     foto: "/path/to/eta.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Theta",
+  //     descripcion: "Análisis de datos financieros para una institución bancaria.",
+  //     nivel: "Experto",
+  //     estrellas: 4,
+  //     foto: "/path/to/theta.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Iota",
+  //     descripcion: "Desarrollo de un sistema de reservas para un hotel.",
+  //     nivel: "Experto",
+  //     estrellas: 4,
+  //     foto: "/path/to/iota.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Kappa",
+  //     descripcion: "Creación de una plataforma de aprendizaje en línea.",
+  //     nivel: "Experto",
+  //     estrellas: 5,
+  //     foto: "/path/to/kappa.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Lambda",
+  //     descripcion: "Desarrollo de una aplicación de seguimiento de salud.",
+  //     nivel: "Experto",
+  //     estrellas: 4,
+  //     foto: "/path/to/lambda.jpg",
+  //   },
+  //   {
+  //     nombre: "Proyecto Mu",
+  //     descripcion: "Implementación de un sistema de gestión de recursos humanos.",
+  //     nivel: "Experto",
+  //     estrellas: 4,
+  //     foto: "/path/to/mu.jpg",
+  //   },
+  // ];
 
   // useEffect(() => {
   //   const fetchproyectos = async () => {
   //     try {
-  //       const data = await proyectoServiceInstance.obtenerProyectos();
+  //       // const data = await proyectoServiceInstance.obtenerProyectos();
+  //       const data = axios.get proyectoServiceInstance.obtenerProyectos();
+        
   //       setProyectos(data);
   //     } catch (error) {
   //       console.error("Error fetching proyectos:", error);
@@ -135,6 +138,17 @@ const ListProyectos = () => {
 
   //   fetchproyectos();
   // }, []);
+  
+  useEffect(() => {
+    axios.get('http://localhost:8085/api/proyecto/all')
+        .then(response => {
+          console.log('Respuesta: ', response);
+          setProyectos(response.data);
+        })
+        .catch(error => {
+            console.error('Error al obtener los usuarios:', error);
+        });
+}, []);
 
   const handleCardClick = (nombre) => {
     navigate(`/proyecto/${encodeURIComponent(nombre)}`);
